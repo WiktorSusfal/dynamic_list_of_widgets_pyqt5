140658742d472dec78495b3f01e37b6bb2b5d4f3
Wiktor Susfal <wiktor.susfal@gmail.com>
2022-08-29 11:08:51 +0200

    Update comments. Update event handler for selected item's changes.
    Now, the selected item changes' event handler is not triggered when other item,
    than the currently selected, is deleted.

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import DLW_GUIList as dlwgl
 from PyQt5 import QtWidgets as qtw
 
 
-# test function to return simple layouts for dynamic list of widgets ('dlwgl.DLW_List()' object)
 def returnSimpleLayout(i: int = 0) -> qtw.QLayout:
+    """
+    Test function to return simple layouts for dynamic list of widgets ('dlwgl.DLW_List()' object)
+    :param i: Custom number to differentiate the layouts - for presentation purposes.
+    :return:
+    """
     name_label = qtw.QLabel('Custom Text Label ' + str(i))
     l_fpath_btn = qtw.QPushButton('Button ' + str(i))
     d_btn = qtw.QPushButton('Del Button')
@@ -17,12 +21,16 @@ def returnSimpleLayout(i: int = 0) -> qtw.QLayout:
     return main_layout
 
 
-# class representing main app window - for presentation and testing purposes
 class MainWindow(qtw.QMainWindow):
+    """
+    Class representing main app window - for presentation and testing purposes
+    """
     def __init__(self):
         super().__init__()
-        # declare 'dlwgl.DLW_List()' object - dynamic list of custom widgets defined by user
+        # Declare 'dlwgl.DLW_List()' object - dynamic list of custom widgets defined by user
         self.list_of_elements = dlwgl.DLW_List()
+        # Subscribe to event handler to receive notifications about selected element's changes.
+        # Pass custom function - in this example to print selected element's id in the screen
         self.list_of_elements.selected_element_changed_handler += self.printCurrentSelectedElementID
 
         # prepare main window to display the list of widgets
@@ -38,6 +46,12 @@ class MainWindow(qtw.QMainWindow):
 
     @staticmethod
     def printCurrentSelectedElementID(element: dlwgl.DLW_ListElement):
+        """
+        Test function called every time there is a notification from DLW_List's event handler - every time the current
+        selected element changes.
+        :param element: Reference to current selected element (DLW_ListElement object)
+        :return:
+        """
         if element is not None:
             print("Current element's id: " + str(element.id))
         else:
